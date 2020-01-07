0abe0ca3921bc4ed9b33979e868ace7babf216fb
xyan264 <Paul.Yan@microchip.com>
2020-01-07 12:27:33 -0800

    Add make depend

--- mingw_install_ssl.sh
+++ mingw_install_ssl.sh
@@ -1,7 +1,8 @@
 wget -c https://www.openssl.org/source/openssl-1.0.2p.tar.gz
 tar -xzvf openssl-1.0.2p.tar.gz
 cd openssl-1.0.2p/
-./config
+./Configure --prefix=/usr/local/openssl no-idea no-mdc2 no-rc5 shared mingw
+make depend
 make
 make test
 sudo make install 
