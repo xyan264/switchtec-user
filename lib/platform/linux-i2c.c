99ae78e3826d89b54d6949af7563c6405be61c67
Paul Yan <Paul.Yan@microchip.com>
2021-04-28 15:14:29 -0700

    Add smbus write

--- lib/platform/linux-i2c.c
+++ lib/platform/linux-i2c.c
@@ -43,7 +43,6 @@
 #include <assert.h>
 #include <string.h>
 
-#include <linux/i2c.h>
 #include <linux/i2c-dev.h>
 
 struct switchtec_i2c {
@@ -318,6 +317,66 @@ i2c_write_fail:
 	return -1;
 }
 
+
+static uint8_t i2c_gas_data_write_smbus(struct switchtec_dev *dev, void __gas *dest,
+				  const void *src, size_t n, uint8_t tag)
+{
+	int ret;
+	struct switchtec_i2c *idev = to_switchtec_i2c(dev);
+
+	struct i2c_msg msg;
+	struct i2c_rdwr_ioctl_data wdata = {
+		.msgs = &msg,
+		.nmsgs = 1,
+	};
+
+	struct {
+		uint8_t command_code;
+		uint8_t byte_count;
+		uint8_t tag;
+		uint32_t offset;
+		uint8_t data[];
+	} __attribute__((packed)) *i2c_data;
+
+	uint32_t gas_addr = (uint32_t)(dest - (void __gas *)dev->gas_map);
+	assert(n <= I2C_MAX_WRITE);
+
+	/* PEC is the last byte */
+	i2c_data = malloc(sizeof(*i2c_data) + n + PEC_BYTE_COUNT);
+
+	i2c_data->command_code = CMD_GAS_WRITE;
+	i2c_data->byte_count = (sizeof(i2c_data->tag)
+				+ sizeof(i2c_data->offset)
+			        + n);
+	i2c_data->tag = tag;
+
+	gas_addr = htobe32(gas_addr);
+	i2c_data->offset = gas_addr;
+	memcpy(&i2c_data->data, src, n);
+	msg.addr = idev->i2c_addr;
+	msg.flags = 0;
+	msg.len = sizeof(*i2c_data) + n + PEC_BYTE_COUNT;
+	msg.buf = (uint8_t *)i2c_data;
+
+	i2c_data->data[n] = i2c_msg_pec(&msg, msg.len - PEC_BYTE_COUNT, 0,
+					 true);
+
+	ret = i2c_smbus_write_block_data(idev->fd, CMD_GAS_WRITE, n + PEC_BYTE_COUNT, ((uint8_t*)i2c_data)+2);
+	if (ret < 0)
+		goto i2c_write_fail;
+
+printf ("sent %d\n", msg.len);
+
+	free(i2c_data);
+	return 0;
+
+i2c_write_fail:
+	free(i2c_data);
+	return -1;
+}
+
+
+
 static uint8_t i2c_gas_write_status_get(struct switchtec_dev *dev,
 					 uint8_t tag)
 {
@@ -368,6 +427,57 @@ static uint8_t i2c_gas_write_status_get(struct switchtec_dev *dev,
 	return -1;
 }
 
+static uint8_t i2c_gas_write_status_get_smbus(struct switchtec_dev *dev,
+					 uint8_t tag)
+{
+	int ret;
+	struct switchtec_i2c *idev = to_switchtec_i2c(dev);
+	struct i2c_msg msgs[2];
+	struct i2c_rdwr_ioctl_data rwdata = {
+		.msgs = msgs,
+		.nmsgs = 2,
+	};
+
+	uint8_t command_code = CMD_GET_WRITE_STATUS;
+	uint8_t rx_buf[3];
+
+	uint8_t msg_0_pec, pec;
+	uint8_t retry_count = 0;
+
+	msgs[0].addr = msgs[1].addr = idev->i2c_addr;
+	msgs[0].flags = 0;
+	msgs[0].len = 1;
+	msgs[0].buf = &command_code;
+
+	msgs[1].flags = I2C_M_RD;
+	msgs[1].len = 3;
+	msgs[1].buf = rx_buf;
+
+	do {
+
+		ret = i2c_smbus_read_block_data(idev->fd, command_code, msgs[1].buf);
+		if (ret < 0) {
+			retry_count++;
+			/* Delay is typically only needed for BL1/2 phase */
+			usleep(2000);
+			continue;
+		}
+printf("heree\n");
+		msg_0_pec = i2c_msg_pec(&msgs[0], msgs[0].len, 0, true);
+		pec = i2c_msg_pec(&msgs[1], msgs[1].len - PEC_BYTE_COUNT,
+				  msg_0_pec, false);
+		if (rx_buf[0] == tag && rx_buf[2] == pec &&
+		    (rx_buf[1] == 0 || rx_buf[1] == GAS_TWI_MRPC_ERR))
+			return rx_buf[1];
+
+		/* Extra delay is typically only needed for BL1/2 phase */
+		usleep(2000);
+		retry_count++;
+	} while(retry_count < MAX_STATUS_GET_RETRY);
+
+	return -1;
+}
+
 static void i2c_gas_write(struct switchtec_dev *dev, void __gas *dest,
 			  const void *src, size_t n)
 {
@@ -378,8 +488,8 @@ static void i2c_gas_write(struct switchtec_dev *dev, void __gas *dest,
 
 	do {
 		tag = get_tag(idev);
-		i2c_gas_data_write(dev, dest, src, n, tag);
-		status = i2c_gas_write_status_get(dev, tag);
+		i2c_gas_data_write_smbus(dev, dest, src, n, tag);
+		status = i2c_gas_write_status_get_smbus(dev, tag);
 		if (status == 0 || status == GAS_TWI_MRPC_ERR)
 			break;
 
